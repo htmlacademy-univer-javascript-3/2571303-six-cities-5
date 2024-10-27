fcc84115b11ecd141b420a69d438a50e94a8976e
Artur Valiullin <ar.valiullin@yandex.ru>
2024-10-27 14:31:15 +0300

    Задача 3: выполнил 1 и 2

---
+++ src/components/app/app.tsx
@@ -0,0 +1,9 @@
+import MainPage from '../../pages/main-page/MainPage.tsx';
+
+function App(): JSX.Element {
+  return (
+    <MainPage/>
+  );
+}
+
+export default App;
---
+++ src/pages/main-page/MainPage.tsx
@@ -0,0 +1,10 @@
+import PlaceCard from '../../components/place-card/place-card.tsx';
+
+function MainPage () {
+  return (
+    <PlaceCard/>
+  );
+
+}
+
+export default MainPage;
