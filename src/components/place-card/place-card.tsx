fd9b655dfdd44edaa63c52865c03ab14afd1dd30
Artur Valiullin <ar.valiullin@yandex.ru>
2024-10-27 16:25:15 +0300

    Формирую динамически список карточек

--- src/components/place-card/place-card.tsx
+++ src/components/place-card/place-card.tsx
@@ -1,18 +1,30 @@
-function PlaceCard() {
+
+type PlaceCardProps = {
+  imageSrc: string;
+  price: number;
+  name: string;
+  placeType: string;
+  rating: string;
+  premium?: boolean;
+}
+
+function PlaceCard({imageSrc, price, name, placeType, rating, premium}: PlaceCardProps) {
   return (
     <article className="cities__card place-card">
-      <div className="place-card__mark">
-        <span>Premium</span>
-      </div>
+      {premium && (
+        <div className="place-card__mark">
+          <span>Premium</span>
+        </div>
+      )}
       <div className="cities__image-wrapper place-card__image-wrapper">
         <a href="#">
-          <img className="place-card__image" src="img/apartment-01.jpg" width="260" height="200" alt="Place image"/>
+          <img className="place-card__image" src={imageSrc} width="260" height="200" alt="Place image"/>
         </a>
       </div>
       <div className="place-card__info">
         <div className="place-card__price-wrapper">
           <div className="place-card__price">
-            <b className="place-card__price-value">&euro;120</b>
+            <b className="place-card__price-value">&euro;{price}</b>
             <span className="place-card__price-text">&#47;&nbsp;night</span>
           </div>
           <button className="place-card__bookmark-button button" type="button">
@@ -24,14 +36,14 @@ function PlaceCard() {
         </div>
         <div className="place-card__rating rating">
           <div className="place-card__stars rating__stars">
-            <span style={{ width: '80%' }}></span>
+            <span style={{ width: rating }}></span>
             <span className="visually-hidden">Rating</span>
           </div>
         </div>
         <h2 className="place-card__name">
-          <a href="#">Beautiful &amp; luxurious apartment at great location</a>
+          <a href="#">{name}</a>
         </h2>
-        <p className="place-card__type">Apartment</p>
+        <p className="place-card__type">{placeType}</p>
       </div>
     </article>
   );
--- src/pages/main-page/MainPage.tsx
+++ src/pages/main-page/MainPage.tsx
@@ -1,10 +1,73 @@
-import PlaceCard from '../../components/place-card/place-card.tsx';
+import PlaceCard from '../../components/place-card/place-card';
+
+type Place = {
+  imageSrc: string;
+  name: string;
+  placeType: string;
+  price: number;
+  rating: number;
+  premium?: boolean;
+};
+
+
+const places: Place[] = [
+  {
+    imageSrc: 'img/apartment-01.jpg',
+    name: 'Beautiful & luxurious apartment at great location',
+    placeType: 'Apartment',
+    price: 120,
+    rating: 100,
+    premium: true
+  },
+  {
+    imageSrc: 'img/room.jpg',
+    name: 'Wood and stone place',
+    placeType: 'Room',
+    price: 80,
+    rating: 80
+  },
+  {
+    imageSrc: 'img/apartment-02.jpg',
+    name: 'Canal View Prinsengracht',
+    placeType: 'Apartment',
+    price: 132,
+    rating: 80
+  },
+  {
+    imageSrc: 'img/apartment-03.jpg',
+    name: 'Nice, cozy, warm big bed apartment',
+    placeType: 'Apartment',
+    price: 180,
+    rating: 100,
+    premium: true
+  },
+  {
+    imageSrc: 'img/room.jpg',
+    name: 'Wood and stone place',
+    placeType: 'Room',
+    price: 80,
+    rating: 80
+  },
+];
+
 
 function MainPage () {
-  return (
-    <PlaceCard/>
-  );
 
+  return (
+    <div className="cities__places-list places__list tabs__content">
+      {places.map((place) => (
+        <PlaceCard
+          key={place.name}
+          imageSrc={place.imageSrc}
+          name={place.name}
+          placeType={place.placeType}
+          price={place.price}
+          rating={`${place.rating}%`}
+          premium={place.premium}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default MainPage;
