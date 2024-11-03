2d987a42f97015cb10ef8f89da1d3c51a001ea6d
Artur Valiullin <ar.valiullin@yandex.ru>
2024-11-04 02:55:50 +0300

    fix favorites

--- src/components/favorite-location-item/favorite-location-item.tsx
+++ src/components/favorite-location-item/favorite-location-item.tsx
@@ -8,18 +8,22 @@ type FavoriteLocationItemProps = {
 
 function FavoriteLocationItem({location, offers}: FavoriteLocationItemProps) {
   return (
-    <div className="favorites__locations locations locations--current">
-      <div className="locations__item">
-        <a className="locations__item-link" href="#">
-          <span>{location}</span>
-        </a>
+    <>
+      <div className="favorites__locations locations locations--current">
+        <div className="locations__item">
+          <a className="locations__item-link" href="#">
+            <span>{location}</span>
+          </a>
+        </div>
       </div>
       <div className="favorites__places">
         {offers.map((offer: Offer) => (
-          <FavoriteOfferCard key={offer.id} imageSrc={offer.imageSrc} price={offer.price} name={offer.name} placeType={offer.type} rating={`${offer.rating}`}/>
+          <FavoriteOfferCard key={offer.id} imageSrc={offer.imageSrc} price={offer.price} name={offer.name}
+            placeType={offer.type} rating={`${offer.rating}`}
+          />
         ))}
       </div>
-    </div>
+    </>
   );
 }
 
--- src/components/favorite-offer-card/favorite-offer-card.tsx
+++ src/components/favorite-offer-card/favorite-offer-card.tsx
@@ -21,13 +21,13 @@ function FavoriteOfferCard({imageSrc, price, name, placeType, rating, premium}: 
           <img className="place-card__image" src={imageSrc} width="150" height="110" alt="Place image"/>
         </a>
       </div>
-      <div className="place-card__info">
+      <div className="favorites__card-info place-card__info">
         <div className="place-card__price-wrapper">
           <div className="place-card__price">
             <b className="place-card__price-value">&euro;{price}</b>
             <span className="place-card__price-text">&#47;&nbsp;night</span>
           </div>
-          <button className="place-card__bookmark-button button" type="button">
+          <button className="place-card__bookmark-button place-card__bookmark-button--active button" type="button">
             <svg className="place-card__bookmark-icon" width="18" height="19">
               <use xlinkHref="#icon-bookmark"></use>
             </svg>
