a0f8b101ad5de40e272262405f90a9b88495101b
Artur Valiullin <ar.valiullin@yandex.ru>
2025-01-03 00:49:21 +0500

    MainPage: include city-list

--- src/components/city-list/city-list.tsx
+++ src/components/city-list/city-list.tsx
@@ -4,27 +4,29 @@ type CityListProps = {
   onCityClick: (city: string) => void;
 };
 
-const CityList: React.FC<CityListProps> = ({ cities, activeCity, onCityClick }) => (
-  <div className="tabs">
-    <section className="locations container">
-      <ul className="locations__list tabs__list">
-        {cities.map((city) => (
-          <li className="locations__item" key={city}>
-            <a
-              className={`locations__item-link tabs__item ${city === activeCity ? 'tabs__item--active' : ''}`}
-              href="#"
-              onClick={(event) => {
-                event.preventDefault();
-                onCityClick(city);
-              }}
-            >
-              <span>{city}</span>
-            </a>
-          </li>
-        ))}
-      </ul>
-    </section>
-  </div>
-);
+function CityList({ cities, activeCity, onCityClick }: CityListProps): JSX.Element {
+  return (
+    <div className="tabs">
+      <section className="locations container">
+        <ul className="locations__list tabs__list">
+          {cities.map((city) => (
+            <li className="locations__item" key={city}>
+              <a
+                className={`locations__item-link tabs__item ${city === activeCity ? 'tabs__item--active' : ''}`}
+                href="#"
+                onClick={(event) => {
+                  event.preventDefault();
+                  onCityClick(city);
+                }}
+              >
+                <span>{city}</span>
+              </a>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </div>
+  );
+}
 
 export default CityList;
--- src/pages/main/main-page.tsx
+++ src/pages/main/main-page.tsx
@@ -1,23 +1,36 @@
-import {Offer} from '../../types/offer.ts';
+import React, { useState } from 'react';
+import { Offer } from '../../types/offer.ts';
 import OfferList from '../../components/offer-list/offer-list.tsx';
 import MapComponent from '../../components/map/map-component.tsx';
-import OFFER_COORDINATES, {CITY} from '../../mocks/points.ts';
-import React from 'react';
+import CityList from '../../components/city-list/city-list.tsx';
+import OFFER_COORDINATES, { CITY } from '../../mocks/points.ts';
 
 type MainPageProps = {
-  offers : Offer[];
-}
+  offers: Offer[];
+};
 
-function MainPage ({offers}: MainPageProps) {
+function MainPage({ offers }: MainPageProps) {
+  //TODO: Для теста компонента CityList
+  const cities = ['Paris', 'Cologne', 'Brussels', 'Amsterdam', 'Hamburg', 'Dusseldorf'];
+  const [activeCity, setActiveCity] = useState(cities[0]);
+  const handleCityClick = (city: string) => {
+    setActiveCity(city);
+  };
+
   return (
     <React.Fragment>
-      <OfferList offers={offers}/>
       <header>
         <h1></h1>
       </header>
       <main>
+        <CityList
+          cities={cities}
+          activeCity={activeCity}
+          onCityClick={handleCityClick}
+        />
+        <OfferList offers={offers} />
         <div className="cities__map">
-          <MapComponent city={CITY} points={OFFER_COORDINATES} selectedPoint={undefined}/>
+          <MapComponent city={CITY} points={OFFER_COORDINATES} selectedPoint={undefined} />
         </div>
       </main>
     </React.Fragment>
