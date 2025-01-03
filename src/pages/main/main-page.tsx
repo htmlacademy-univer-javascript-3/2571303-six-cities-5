538c4d2176f1fdf700b8196eb5b638d9d0ed4005
Artur Valiullin <ar.valiullin@yandex.ru>
2025-01-03 23:13:12 +0500

    components: add Header

--- src/pages/main/main-page.tsx
+++ src/pages/main/main-page.tsx
@@ -8,6 +8,7 @@ import CityList from '../../components/city-list/city-list';
 import {City, Offer} from '../../types/offer.ts';
 import { CITIES } from '../../consts.ts';
 import { CITY } from '../../mocks/points.ts';
+import Header from '../../components/header/header.tsx';
 
 type MainPageProps = {
   offers: Offer[];
@@ -42,35 +43,7 @@ function MainPage({offers}: MainPageProps) {
 
   return (
     <div className="page page--gray page--main">
-      <header className="header">
-        <div className="container">
-          <div className="header__wrapper">
-            <div className="header__left">
-              <a className="header__logo-link header__logo-link--active">
-                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41"/>
-              </a>
-            </div>
-            <nav className="header__nav">
-              <ul className="header__nav-list">
-                <li className="header__nav-item user">
-                  <a className="header__nav-link header__nav-link--profile" href="#">
-                    <div className="header__avatar-wrapper user__avatar-wrapper">
-                    </div>
-                    <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
-                    <span className="header__favorite-count">3</span>
-                  </a>
-                </li>
-                <li className="header__nav-item">
-                  <a className="header__nav-link" href="#">
-                    <span className="header__signout">Sign out</span>
-                  </a>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        </div>
-      </header>
-
+      <Header/>
       <main className="page__main page__main--index">
         <CityList cities={CITIES} activeCity={activeCity} onCityClick={handleCityClick}/>
         <div className="cities">
--- src/pages/offer/offer-page.tsx
+++ src/pages/offer/offer-page.tsx
@@ -7,6 +7,7 @@ import { CITY, OFFER_COORDINATES } from '../../mocks/points';
 import MapComponent from '../../components/map/map-component';
 import ReviewList from '../../components/reviews-list/reviews-list.tsx';
 import { REVIEWS } from '../../mocks/reviews';
+import Header from '../../components/header/header.tsx';
 
 type OfferPageProps = {
   offers: Offer[];
@@ -27,34 +28,7 @@ function OfferPage({ offers }: OfferPageProps) {
   const features = offer.features || [];
   return (
     <div className="page">
-      <header className="header">
-        <div className="container">
-          <div className="header__wrapper">
-            <div className="header__left">
-              <a className="header__logo-link" href="main.html">
-                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41" />
-              </a>
-            </div>
-            <nav className="header__nav">
-              <ul className="header__nav-list">
-                <li className="header__nav-item user">
-                  <a className="header__nav-link header__nav-link--profile" href="#">
-                    <div className="header__avatar-wrapper user__avatar-wrapper"></div>
-                    <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
-                    <span className="header__favorite-count">3</span>
-                  </a>
-                </li>
-                <li className="header__nav-item">
-                  <a className="header__nav-link" href="#">
-                    <span className="header__signout">Sign out</span>
-                  </a>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        </div>
-      </header>
-
+      <Header/>
       <main className="page__main page__main--offer">
         <section className="offer">
           <div className="offer__gallery-container container">
