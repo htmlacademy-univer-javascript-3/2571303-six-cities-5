ab0650c564aa5a8d564d4c34ff1b744a10f52e77
Artur Valiullin <ar.valiullin@yandex.ru>
2025-01-04 01:49:46 +0500

    store: update

--- src/store/action.ts
+++ src/store/action.ts
@@ -1,17 +1,15 @@
 import { createAction } from '@reduxjs/toolkit';
-import axios from 'axios';
-import { AppDispatch } from './index';
+import { AppDispatch, ThunkExtraArgument } from './index';
 import { Offer, City } from '../types/offer.ts';
-import { API_URL } from '../consts.ts';
-import {setCity, setOffers} from './reducer.ts';
+import { setCity, setOffers } from './reducer.ts';
 
 export const setLoading = createAction<boolean>('offers/setLoading');
 export const setError = createAction<string | null>('offers/setError');
 
-export const fetchOffersByCity = (city: City) => async (dispatch: AppDispatch) => {
+export const fetchOffersByCity = (city: City) => async (dispatch: AppDispatch, _getState: never, axiosInstance: ThunkExtraArgument) => {
   dispatch(setLoading(true));
   try {
-    const response = await axios.get<Offer[]>(`${API_URL}/offers`);
+    const response = await axiosInstance.get<Offer[]>('/offers');
     const allOffers = response.data;
 
     const filteredOffers = allOffers.filter((offer) => offer.city.name === city.name);
