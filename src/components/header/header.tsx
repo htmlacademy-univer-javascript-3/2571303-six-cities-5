d29dbf99279cf192c41a2b3a396d1f5ea8dfa6a9
Artur Valiullin <ar.valiullin@yandex.ru>
2025-01-04 03:17:31 +0500

    header.tsx: impl sign in

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,4 +1,20 @@
+import { useSelector } from 'react-redux';
+import { RootState } from '../../store';
+import { useNavigate } from 'react-router-dom';
+
 function Header() {
+  const authorizationStatus = useSelector((state: RootState) => state.offers.authorizationStatus);
+  const navigate = useNavigate();
+
+  const handleSignInClick = () => {
+    navigate('/login');
+  };
+
+  const handleSignOutClick = () => {
+    // eslint-disable-next-line no-console
+    console.log('User signed out');
+  };
+
   return (
     <header className="header">
       <div className="container">
@@ -16,20 +32,34 @@ function Header() {
           </div>
           <nav className="header__nav">
             <ul className="header__nav-list">
-              <li className="header__nav-item user">
-                <a className="header__nav-link header__nav-link--profile" href="#">
-                  <div className="header__avatar-wrapper user__avatar-wrapper"></div>
-                  <span className="header__user-name user__name">
-                    Oliver.conner@gmail.com
-                  </span>
-                  <span className="header__favorite-count">3</span>
-                </a>
-              </li>
-              <li className="header__nav-item">
-                <a className="header__nav-link" href="#">
-                  <span className="header__signout">Sign out</span>
-                </a>
-              </li>
+              {authorizationStatus ? (
+                <>
+                  <li className="header__nav-item user">
+                    <a className="header__nav-link header__nav-link--profile" href="#">
+                      <div className="header__avatar-wrapper user__avatar-wrapper"></div>
+                      <span className="header__user-name user__name">
+                        Oliver.conner@gmail.com
+                      </span>
+                      <span className="header__favorite-count">3</span>
+                    </a>
+                  </li>
+                  <li className="header__nav-item">
+                    <button className="header__nav-link" onClick={handleSignOutClick}>
+                      <span className="header__signout">Sign out</span>
+                    </button>
+                  </li>
+                </>
+              ) : (
+                <li className="header__nav-item user">
+                  <button
+                    className="header__nav-link header__nav-link--profile"
+                    onClick={handleSignInClick}
+                  >
+                    <div className="header__avatar-wrapper user__avatar-wrapper"></div>
+                    <span className="header__login">Sign in</span>
+                  </button>
+                </li>
+              )}
             </ul>
           </nav>
         </div>
