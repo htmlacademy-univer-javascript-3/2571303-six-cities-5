11768dab0fdadc09219c4626097ff3f26acbd3cb
Artur Valiullin <ar.valiullin@yandex.ru>
2025-01-08 23:26:48 +0300

    FavaritesPage: update

--- src/pages/favorites/favorites-page.tsx
+++ src/pages/favorites/favorites-page.tsx
@@ -3,10 +3,10 @@ import { Offer } from '../../types/offer.ts';
 import FavoriteLocationItem from '../../components/favorite-location-item/favorite-location-item.tsx';
 import Header from '../../components/header/header.tsx';
 import Footer from '../../components/footer/footer.tsx';
-import { fetchFavoriteOffers } from '../../api/api.ts';
 import Spinner from '../../components/spinner/spinner.tsx';
-import {setFavoritesCount} from '../../store/slices/offers-slice.ts';
-import {useDispatch} from 'react-redux';
+import { fetchFavoriteOffers } from '../../api/api.ts';
+import { setFavoritesCount } from '../../store/slices/offers-slice.ts';
+import { useDispatch } from 'react-redux';
 
 type GroupedOffers = Record<string, Offer[]>;
 
@@ -27,6 +27,7 @@ function FavoritesPage() {
   const [offers, setOffers] = useState<Offer[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const dispatch = useDispatch();
+
   useEffect(() => {
     const loadOffers = async () => {
       try {
@@ -40,15 +41,36 @@ function FavoritesPage() {
     };
 
     loadOffers();
-  }, []);
+  }, [dispatch]);
 
   const groupedOffers = groupOffersByCity(offers);
 
   if (isLoading) {
+    return <Spinner />;
+  }
+
+  if (offers.length === 0) {
     return (
-      <Spinner/>
+      <div className="page page--favorites-empty">
+        <Header />
+        <main className="page__main page__main--favorites page__main--favorites-empty">
+          <div className="page__favorites-container container">
+            <section className="favorites favorites--empty">
+              <h1 className="visually-hidden">Favorites (empty)</h1>
+              <div className="favorites__status-wrapper">
+                <b className="favorites__status">Nothing yet saved.</b>
+                <p className="favorites__status-description">
+                  Save properties to narrow down search or plan your future trips.
+                </p>
+              </div>
+            </section>
+          </div>
+        </main>
+        <Footer />
+      </div>
     );
   }
+
   return (
     <div className="page">
       <Header />
